8926026d6ddfdcedd182776e841ca1db635834a2
Himanshu Raj <rhimanshu.2004@gmail.com>
2024-06-30 11:12:13 +0530

    added cp template and snippets

---
+++ cp/ws.cpp
@@ -0,0 +1,30 @@
+/**
+ *  author: rahisenpai
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define CEIL(m,n) ((m)+(n)-1)/(n)
+const int mod = 1000000007;
+inline int posmod(int k){
+    return ((k % mod) + mod) % mod;
+}
+//sfpbds, sfbinexp, sfgcd, sfsieve, sfncr, sfdfs, sfbfs
+//sfdijkstra, sfbellmanford, sfflloydwarshall, sfdsu
+
+void solve() {
+    int n;
+    cin>>n;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int t; cin>>t;
+    while(t--) solve();
+    return 0;
+}
